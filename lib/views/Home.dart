d656eb4334d10d3e685eb9ad42964b633524fc48
mllusar <mathis.llusar@epitech.eu>
2021-10-13 00:46:53 +0200

    Basic Home pge interface

--- lib/views/Home.dart
+++ lib/views/Home.dart
@@ -1,6 +1,7 @@
 import 'package:app/controllers/Controller.dart';
 import 'package:mvc_application/controller.dart';
 import 'package:mvc_application/view.dart';
+import 'package:flutter/material.dart';
 
 //view
 class Home extends StatefulWidget {
@@ -19,8 +20,48 @@ class _Home extends StateMVC<Home> {
   @override
   Widget build(BuildContext context) =>
       Scaffold(
+
           appBar: AppBar(
-              title: Text(widget.title)
-          )
+              title: Text(widget.title),
+              leading: Icon(Icons.menu),
+              actions: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40),
+                  child: IconButton(icon: Icon(Icons.search), onPressed: () {}),
+                ),
+              ],
+              //backgroundColor: Colors.purple,
+          ),
+
+          drawer: Drawer(
+            child: Column(
+              children: <Widget>[
+                UserAccountsDrawerHeader(
+                    accountName: Text("accountName"),
+                    accountEmail: Text("accountEmail"),
+                    currentAccountPicture: CircleAvatar(child: Text("P"))
+                ),
+
+                ListTile(
+                  leading: Icon(Icons.home),
+                  title: Text("Home") //Faire un Boutton
+                )
+              ],
+            ),
+          ),
+          bottomNavigationBar: BottomAppBar(
+            child: Row(
+              children: [
+                IconButton(icon: Icon(Icons.filter_list_rounded), onPressed: () {}),
+                Spacer(),
+                IconButton(icon: Icon(Icons.settings), onPressed: () {}),
+              ],
+            ),
+            shape: CircularNotchedRectangle(),
+          ),
+
+          floatingActionButton:
+          FloatingActionButton(child: Icon(Icons.add), onPressed: () {}),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       );
 }
