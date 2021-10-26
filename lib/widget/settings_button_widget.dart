3f87cee99f6fca3c0cbb272e5844488f67c9087d
mllusar <mathis.llusar@epitech.eu>
2021-10-26 17:55:26 +0200

    Modifying settings switch buttons

--- lib/widget/settings_button_widget.dart
+++ lib/widget/settings_button_widget.dart
@@ -13,8 +13,12 @@ class SettingsButtonWidget extends StatefulWidget {
 
 class _SettingsButtonWidget extends State<SettingsButtonWidget> {
   final RedditClient client = RedditClient();
-  bool _is_over_18 = true;
+  bool _is_over_18 = false;
   bool _is_hidden = false;
+  bool _is_beta = false;
+  bool _night_mode = false;
+  bool _email_chat_r = false;
+  bool _email_comment_r = false;
 
   @override
   Widget build(BuildContext context) {
@@ -22,112 +26,55 @@ class _SettingsButtonWidget extends State<SettingsButtonWidget> {
       children: [
         Column (children: [
             Row(
-              children: const <Widget>[
-                Padding(padding: EdgeInsets.all(16.0),child: Text("Profile Picture")),
-                Expanded(
-                    child: Divider()
-                ),
-              ]
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB( 50, 0, 50, 0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  CircleAvatar(
-                      child: client.isConnected ? Image.network(
-                          client.me!.iconImg!) : const Text("P"),
-                      backgroundColor: const Color.fromRGBO(0, 0, 0, 0)
-                  ),
-                  ElevatedButton.icon(
-                    onPressed: () {
-                      // Respond to button press
-                    },
-                    icon: const Icon(Icons.edit, size: 18),
-                    label: const Text("MODIFY"),
-                  )
-                ]
-              ),
-            ),
-            Row(
                 children: const <Widget>[
-                  Padding(padding: EdgeInsets.all(16.0),child: Text("Banner")),
+                  Padding(padding: EdgeInsets.all(16.0),child: Text("Night Mode")),
                   Expanded(
                       child: Divider()
                   ),
                 ]
             ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB( 50, 20, 50, 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            Row (
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  client.me?.bannerImg != null ?
-                  Image.network(
-                    client.me!.bannerImg!,
-                    fit: BoxFit.fill,
-                    height: 100,
-                    width: 150) :
-                  Image.asset(
-                      'assets/default_image.png',
-                      fit: BoxFit.fill,
-                      height: 100,
-                      width: 150),
-                  ElevatedButton.icon(
-                    onPressed: () {
-                      // Respond to button press
+                  Text('Enable Night Mode in preferences'),
+                  Switch(
+                    value: _night_mode,
+                    activeColor: Color(0xFF6200EE),
+                    inactiveThumbColor: Color(0x33333333),
+                    onChanged: (bool value)  {
+                      setState(() {
+                        _night_mode = value;
+                        // fonction à appeler avec value en param
+                      });
                     },
-                    icon: const Icon(Icons.edit, size: 18),
-                    label: const Text("MODIFY"),
                   ),
                 ]
-              ),
             ),
-
             Row(
                 children: const <Widget>[
-                  Padding(padding: EdgeInsets.all(16.0),child: Text("Display Name")),
+                  Padding(padding: EdgeInsets.all(16.0),child: Text("Beta interface")),
                   Expanded(
                       child: Divider()
                   ),
                 ]
             ),
-            Container(
-              padding: EdgeInsets.all(12),
-              child: TextFormField(
-                initialValue: '',
-                maxLines: 1,
-                decoration: const InputDecoration(
-                  labelText: 'Your name here',
-                  border: OutlineInputBorder(),
-                  contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                  isDense: true,
-                )
-              ),
+            Row (
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('Enable the Beta view interface'),
+                  Switch(
+                    value: _is_beta,
+                    activeColor: Color(0xFF6200EE),
+                    inactiveThumbColor: Color(0x33333333),
+                    onChanged: (bool value)  {
+                      setState(() {
+                        _is_beta = value;
+                        // fonction à appeler avec value en param
+                      });
+                    },
+                  ),
+                ]
             ),
-
-          Row(
-              children: const <Widget>[
-                Padding(padding: EdgeInsets.all(16.0),child: Text("Biography")),
-                Expanded(
-                    child: Divider()
-                ),
-              ]
-          ),
-          Container(
-            padding: EdgeInsets.all(12),
-            child: TextFormField (
-                initialValue: '',
-                maxLines: 1,
-                decoration: const InputDecoration(
-                  labelText: '200 characters maximum',
-                  border: OutlineInputBorder(),
-                  contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                  isDense: true,
-                )
-            ),
-          ),
-
             Row(
                 children: const <Widget>[
                   Padding(padding: EdgeInsets.all(16.0),child: Text("NSFW content")),
@@ -177,7 +124,49 @@ class _SettingsButtonWidget extends State<SettingsButtonWidget> {
                     },
                   ),
                 ]
-            )
+            ),
+            Row(
+                children: const <Widget>[
+                  Padding(padding: EdgeInsets.all(16.0),child: Text("Email")),
+                  Expanded(
+                      child: Divider()
+                  ),
+                ]
+            ),
+            Row (
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('Email Chat request'),
+                  Switch(
+                    value: _email_chat_r,
+                    activeColor: Color(0xFF6200EE),
+                    inactiveThumbColor: Color(0x33333333),
+                    onChanged: (bool value)  {
+                      setState(() {
+                        _email_chat_r = value;
+                        // fonction à appeler avec value en param
+                      });
+                    },
+                  ),
+                ]
+            ),
+            Row (
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('Email Comment reply'),
+                  Switch(
+                    value: _email_comment_r,
+                    activeColor: Color(0xFF6200EE),
+                    inactiveThumbColor: Color(0x33333333),
+                    onChanged: (bool value)  {
+                      setState(() {
+                        _email_comment_r = value;
+                        // fonction à appeler avec value en param
+                      });
+                    },
+                  ),
+                ]
+            ),
           ]
         )
       ]
