233a4ee11b3eb9e9833b8e8dcec0a6d210b5955b
mllusar <mathis.llusar@epitech.eu>
2021-10-28 17:46:19 +0200

    Adding searchBar proto

--- lib/widget/nav_top_bar_widget.dart
+++ lib/widget/nav_top_bar_widget.dart
@@ -14,12 +14,83 @@ class NavigationTopBarWidget extends StatelessWidget implements PreferredSizeWid
       actions: [
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: IconButton(icon: const Icon(Icons.search), onPressed: () {}),
+          child: IconButton(icon: const Icon(Icons.search), onPressed: () {
+            showSearch(context: context, delegate: ExSearch());
+          }),
         ),
       ]
     );
   }
+  @override
+  
+  Size get preferredSize => const Size.fromHeight(50);
+}
+
+class ExSearch extends SearchDelegate<String> {
+  final exemple = [
+    'r/abc',
+    'r/edf',
+    'r/opm',
+  ];
+
+  final recent = [
+    'r/abc',
+  ];
 
   @override
-  Size get preferredSize => const Size.fromHeight(50);
+  List<Widget>? buildActions(BuildContext context) {
+    IconButton(
+      icon: Icon(Icons.clear),
+      onPressed: () {},
+    );
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {},
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) =>
+      Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.star, size: 120),
+              SizedBox(height: 48),
+              Text(
+                  query,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 64,
+                    fontWeight: FontWeight.bold,
+                  )
+              )
+            ],
+          )
+      );
+
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final sugg = recent;
+
+    return buildSuggestionsSucces(sugg);
+  }
+
+  Widget buildSuggestionsSucces(List<String> sugg) =>
+      ListView.builder(
+        itemCount: sugg.length,
+        itemBuilder: (context, index) {
+          final suggestion = sugg[index];
+
+          return ListTile(
+            leading: Icon(Icons.location_city),
+            title: Text(suggestion),
+          );
+        },
+      );
 }
