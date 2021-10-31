6a30463b533d0f1392131ca70001487b2b4c6caf
Melefo <melefo@melefo.xyz>
2021-10-31 14:31:08 +0100

    Remove unused import

--- lib/views/settings.dart
+++ lib/views/settings.dart
@@ -4,7 +4,6 @@ import 'package:app/widget/nav_drawer_widget.dart';
 import 'package:app/widget/nav_fab_button_widget.dart';
 import 'package:app/widget/nav_top_bar_widget.dart';
 import 'package:app/widget/settings_button_widget.dart';
-import 'package:flutter/services.dart';
 import 'package:mvc_application/controller.dart';
 import 'package:mvc_application/view.dart';
 import 'package:flutter/material.dart';
